8938781e7ea6bc679b83092370d4624cb87eb24b
luisp <luis.palomero.lopez.armentia@gmail.com>
2019-12-13 10:46:10 +0100

    Build basic documentation

--- R/genScore.R
+++ R/genScore.R
@@ -1,3 +1,19 @@
+#' Genetic Score metric
+#' 
+#' @author Karl W Broman, \email{broman@@wisc.edu}
+#'
+#' This metric "collapses" two-dimensional values into a one dimensional. 
+#' This distance is related to the inverse diagonal from a 2D space metric varA/varB
+#'
+#' @param varA array of "X" dimension values.
+#' @param varB array of "Y" dimension values
+#'
+#' @return Array with the antisense distance metric
+#'
+#' @examples
+#' genScore(c(1,2,3,4), c(4,3,2,1))
+#'
+#' @export
 genScore <- function(varA, varB){
   marginsA = c(max = max(varA), min=  min(varA))
   marginsB = c(max = max(varB), min = min(varB))
